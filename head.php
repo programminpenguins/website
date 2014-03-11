169609d2225492e72c21905d912d8ba280b2bc8d
josmcg <joshuawmcgrath@gmail.com>
2014-03-10 22:24:43 -0500

    add in viewport tag to aid in responsive design

--- head.php
+++ head.php
@@ -6,5 +6,6 @@
 
 <link rel="stylesheet" href="dependencies/stylesheets/mobile.css" type="text/css" />
 <script src="dependencies/scripts/mobile.js"></script>
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
 <link rel="stylesheet" href="dependencies/stylesheets/penguin.css" type="text/css" /> <!-- keep this one after all others, so it takes precidence -->
